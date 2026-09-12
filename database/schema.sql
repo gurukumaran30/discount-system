1b9647793a6e47e5d9b5e27d5a4ee2789ceaa2c3
guru <gurukum7@gmail.com>
2026-09-12 23:47:18 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE discount_system;
+USE discount_system;
 
+CREATE TABLE users (...);
+
+CREATE TABLE products (...);
+
+CREATE TABLE discounts (...);
+
+CREATE TABLE purchases (...);
